2a15925d5d18d791aa9f65ec363a46382c337253
BaronVonScrub <a.sakovits@outlook.com>
2020-02-28 09:49:39 +1030

    Grid Toggle
    Added grid toggle via Space.

---
+++ Assets/Commands/GridToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridToggle : MonoBehaviour
+{
+    public void Toggle()
+    {
+        GameObject floor = GameObject.FindGameObjectWithTag("Floor");
+        Renderer renderer = floor.GetComponent<Renderer>();
+        Material material = renderer.material;
+
+        if (material.shader != Shader.Find("Custom/GroundShader"))
+            material.shader = Shader.Find("Custom/GroundShader");
+        else
+            material.shader = Shader.Find("Custom/GridShader");
+    }
+}
